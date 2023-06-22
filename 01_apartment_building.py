e2873e1bea08f7410413c1f38182c8208a68fb57
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2023-06-22 21:21:49 +0200

    Part 2 -- calculate position at which Santa first enters the basement

--- 01_apartment_building.py
+++ 01_apartment_building.py
@@ -5,12 +5,33 @@
 
 # get input data
 
-import_input_data = None
+input_data = None
 
 with open("01_apartment_building_input.txt") as input:
-    import_input_data = list(input.readlines()[0])
+    input_data = list(input.readlines()[0])
 
 # calculate the floor Santa will end up on
 
-end_floor = abs(import_input_data.count("(") - import_input_data.count(")"))
+end_floor = input_data.count("(") - input_data.count(")")
 print("\nSanta will end up on floor", end_floor)
+
+
+
+#****************** Part 2 *****
+
+
+# calculate the position where Santa enters the basement
+
+position = None
+floor = 0
+
+for i in range(len(input_data)):
+    if input_data[i] == "(":
+        floor += 1
+    else:
+        floor -= 1
+    if floor == -1:
+        position = i+1
+        break
+
+print("\nThe position at which Santa first enters the basement is", position)
