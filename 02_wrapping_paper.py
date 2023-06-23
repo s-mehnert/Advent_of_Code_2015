57321308345097c40d0591c2bf29156baec40e0e
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2023-06-23 09:25:10 +0200

    Part 2 -- calculate ribbon length

--- 02_wrapping_paper.py
+++ 02_wrapping_paper.py
@@ -15,13 +15,27 @@ dimensions_int = [[int(item) for item in dimension] for dimension in dimensions]
 
 # calculate square feet of wrapping paper needed
 
-result = 0
+paper_needed = 0
 for box in dimensions_int:
     side_1 = box[0]*box[1]
     side_2 = box[1]*box[2]
     side_3 = box[2]*box[0]
-    result += sum([2*side_1, 2*side_2, 2*side_3, min(side_1, side_2, side_3)])
+    paper_needed += sum([2*side_1, 2*side_2, 2*side_3, min(side_1, side_2, side_3)])
 
-print(f"\nIn total the elves need {result} square feet of wrapping paper.")
+print(f"\nIn total the elves need {paper_needed} square feet of wrapping paper.")
 
-        
+
+
+#****************** Part 2 *****
+
+
+# calculate feet of ribbon needed
+
+ribbon_needed = 0
+for box in dimensions_int:
+    bow = box[0]*box[1]*box[2]
+    box.remove(max(box))
+    ribbon_needed += (bow + sum(box)*2)
+
+print(f"\nIn total the elves need {ribbon_needed} feet of ribbon.")
+
