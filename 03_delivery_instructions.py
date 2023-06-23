ab5a701190885d0902893868b67f0ea728459b01
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2023-06-23 09:57:54 +0200

    create function to decipher instructions -- in progress, failing

--- 03_delivery_instructions.py
+++ 03_delivery_instructions.py
@@ -12,18 +12,62 @@ with open("03_delivery_instructions_input.txt") as input:
 
 print(input_data)
 
-# decipher instructions
-
 # create grid in which Santa moves
 
 class Grid():
     def __init__(self, rows, cols):
         self.matrix = [[(i, j) for j in range(cols)] for i in range(rows)]
-        self.visited = list()
+        self.santa_pos = self.matrix[rows//2][cols//2]
+        self.visited = [self.santa_pos]
 
 # create method to mark houses as visited
     def mark_visited(self, position):
-        self.visited.append(position)
+        if position not in self.visited:
+            self.visited.append(position)
+            self.matrix[position[0]][position[1]] = " X "
+
+# create method to visualize the grid
+    def visualize(self):
+        for row in self.matrix:
+            print(row)
+
+# create method to move Santa around
+    def move_north():
+        self.santa_pos[0] -= 1
+    
+    def move_south():
+        self.santa_pos[0] += 1
+    
+    def move_west():
+        self.santa_pos[1] -= 1
+    
+    def move_east():
+        self.santa_pos[1] += 1
 
 # count houses that received at least one present
 
+
+# decipher instructions
+
+def decipher_instructions(grid, instruction_string): # in progress, correct error
+    for instr in instruction_string:
+        if instr == "^":
+            grid.move_north()
+        elif instr == "v":
+            grid.move_south()
+        elif instr == "<":
+            grid.move_west()
+        elif instr == ">":
+            grid.move_east()
+        else:
+            print("Invalid instruction.")
+        grid.mark_visited(grid.santa_pos)
+
+
+# Testing
+
+house_grid = Grid(10, 10)
+house_grid.visualize()
+
+decipher_instructions(house_grid, input_data)
+house_grid.visualize()
