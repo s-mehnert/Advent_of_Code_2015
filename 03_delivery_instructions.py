02a43e9656ec2b6f319ac6eca42632931cd8c95e
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2023-06-23 09:31:49 +0200

    Day 3: import and format data

---
+++ 03_delivery_instructions.py
@@ -0,0 +1,16 @@
+#***** ADVENT OF CODE 2015 *****
+#************ DAY 3 ************
+#****************** Part 1 *****
+
+
+# get input data
+
+input_data = None
+
+with open("03_delivery_instructions_input.txt") as input:
+    input_data = input.readlines()[0]
+
+print(input_data)
+
+# decipher instructions
+
